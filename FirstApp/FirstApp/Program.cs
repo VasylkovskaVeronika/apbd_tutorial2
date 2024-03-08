ffa5932d24dd9fcbea07433fc5fa8c69a5453371
Veronika <veronika.novosad4@gmail.com>
2024-03-08 18:47:02 +0100

    add GetAverage method

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Modified");
-Console.WriteLine("Hello, World!");
+
+static double GetAverage(int[] arr)
+{
+    double res = 0;
+    foreach (var num in arr) {
+        res += num;
+    }
+    res/= arr.Length;
+    return res;
+}
