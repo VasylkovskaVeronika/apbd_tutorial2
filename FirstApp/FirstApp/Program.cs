dd5600e8911cfddfae02dce37b062bd0683b4c88
Veronika <veronika.novosad4@gmail.com>
2024-03-12 15:59:17 +0100

    Create feature-new

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -1,12 +1,1 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-static double GetMax(int[] arr)
-{
-    double res = 0;
-    foreach (var num in arr) {
-        if(res<num)
-            res = num;
-    }
-    return res;
-}
