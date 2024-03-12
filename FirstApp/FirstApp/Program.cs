11a607e0930c33dfd0d64c3112de5a7f3748958a
Veronika <veronika.novosad4@gmail.com>
2024-03-12 19:49:23 +0100

    Updated with adding all new distinct methods

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -1,12 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+
 static double GetMax(int[] array)
 {
     double res = 0;
     foreach (var num in array) {
-        if(res<num)
+        if (res < num)
             res = num;
     }
     return res;
+}
+
+static double GetAverage(int[] array)
+{
+    double result = 0;
+    foreach (var n in array)
+    {
+        result += n;
+
+    }
+    result /= array.Length;
+    return result;
 }
