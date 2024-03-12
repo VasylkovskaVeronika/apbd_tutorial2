32e7528ffdf0ce095cfe096d472b4b60f4c5b2d8
Veronika <veronika.novosad4@gmail.com>
2024-03-12 16:49:59 +0100

    Code about GetAverage returned)

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -1,1 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static double GetAverage(int[] arr)
+{
+    double res = 0;
+    foreach (var num in arr)
+    {
+        res += num;
+    }
+    res /= arr.Length;
+    return res;
+}
